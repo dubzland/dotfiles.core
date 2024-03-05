42391700e024834d1082d9d464c58ca13c7abbfa
Josh Williams <jdubz@dubzland.com>
2024-03-05 07:31:46 -0600

    Rewrite hook scripts and remove install/uninstall

--- .config/tinted-theming/hooks/alacritty.sh
+++ .config/tinted-theming/hooks/alacritty.sh
@@ -1,25 +1,47 @@
 #!/usr/bin/env bash
 
-XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+# Hook script to update alacritty colors when base16 theme switches.
+# Run in a subshell to avoid mangling the parent.
+(
 
-function die() {
-	printf "%s\n" "$*" >&2
-}
+	# Enable tracing if the DEBUG environment variable is set
+	if [[ ${DEBUG} =~ ^1|yes|true$ ]]; then
+		set -o xtrace
+	fi
 
-# base16-alacritty theme directory
-ALACRITTY_THEME_DIR="$XDG_CONFIG_HOME/tinted-theming/base16-alacritty/colors"
-[[ -d "${ALACRITTY_THEME_DIR}" ]] || die "alacrity theme directory not found"
+	set -o errexit
+	set -o nounset
+	set -o pipefail
 
-# alacrity configuration directory
-ALACRITTY_CONFIG_DIR="$XDG_CONFIG_HOME/alacritty"
-[[ -d "${ALACRITTY_CONFIG_DIR}" ]] || die "alacritty config directory not found"
+	function die() {
+		if [[ $# -eq 1 ]]; then
+			printf '%s\n' "$1"
+			exit 1
+		fi
 
-# Current base16 theme
-[[ -n "${BASE16_THEME}" ]] || die "No base16 theme set"
+		if [[ ${2-} =~ ^[0-9]+$ ]]; then
+			printf '%b\n' "$1"
+			exit $2
+		fi
+	}
+
+	XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+
+	# base16-alacritty theme directory
+	alacritty_theme_dir="$XDG_CONFIG_HOME/tinted-theming/base16-alacritty/colors"
+	[[ -d "${alacritty_theme_dir}" ]] || die "alacrity theme directory not found"
+
+	# alacrity configuration directory
+	alacritty_config_dir="$XDG_CONFIG_HOME/alacritty"
+	[[ -d "${alacritty_config_dir}" ]] || die "alacritty config directory not found"
+
+	# Current base16 theme
+	[[ -n "${BASE16_THEME}" ]] || die "No base16 theme set"
 
 
-# Make sure a matching theme exists
-ALACRITTY_THEME_FILE="${ALACRITTY_THEME_DIR}/base16-${BASE16_THEME}-256.toml"
-[[ -r "${ALACRITTY_THEME_FILE}" ]] || die "alacritty theme ${ALACRITTY_THEME_FILE} not found"
+	# Make sure a matching theme exists
+	alacritty_theme_file="${alacritty_theme_dir}/base16-${BASE16_THEME}-256.toml"
+	[[ -r "${alacritty_theme_file}" ]] || die "alacritty theme ${alacritty_theme_file} not found"
 
-ln -sf "$ALACRITTY_THEME_FILE" "${ALACRITTY_CONFIG_DIR}/theme.toml"
+	ln -sf "$alacritty_theme_file" "${alacritty_config_dir}/theme.toml"
+)
--- .config/tinted-theming/hooks/tmux.sh
+++ .config/tinted-theming/hooks/tmux.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Hook script to update tmux sessions when base16 theme switches.
+
 if pgrep tmux >/dev/null; then
 	tmux setenv -g BASE16_THEME "$BASE16_THEME"
     tmux source-file "$(tmux display-message -p "#{config_files}")"
