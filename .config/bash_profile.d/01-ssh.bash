b491b4b3de532bc8fe2e4c56b89ad4574111d327
Josh Williams <jdubz@dubzland.com>
2024-04-30 07:14:00 -0500

    Make the ssh agent logic a bit more robust

--- .config/bash_profile.d/01-ssh.bash
+++ .config/bash_profile.d/01-ssh.bash
@@ -1,12 +1,23 @@
 #
 # SSH agent setup
 #
-if [[ -z "$SSH_AUTH_SOCK" ]]; then
-    eval $(ssh-agent)
+if [[ -z "${SSH_AUTH_SOCK}" ]]; then
+    # No agent running.  Start a new one
+    eval "$(ssh-agent)"
 else
-    if [[ -h $HOME/.ssh/ssh_auth_sock ]]; then
-        if [[ "$SSH_AUTH_SOCK" -ef "$HOME/.ssh/ssh_auth_sock" ]]; then
-            export SSH_AUTH_SOCK=$HOME/.ssh/ssh_auth_sock
+    # Agent available (possibly via forwarding)
+    # Check for the existence of the ssh agent symlink set via rc
+    if [[ -L ${HOME}/.ssh/ssh_auth_sock ]]; then
+        # Symlink exists. Is it valid?
+        if [[ -e ${HOME}/.ssh/ssh_auth_sock ]]; then
+            # Symlink is valid. Does it point to our instance?
+            if [[ "${SSH_AUTH_SOCK}" -ef "${HOME}/.ssh/ssh_auth_sock" ]]; then
+                # It does. Update environment to point to the symlink
+                export SSH_AUTH_SOCK=${HOME}/.ssh/ssh_auth_sock
+            fi
+        else
+            # Symlink is invalid (dead session)
+            rm "${HOME}/.ssh/ssh_auth_sock"
         fi
     fi
 fi
